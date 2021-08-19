04291dae9bf1db76f73c44110454ab7ba650891c
bartimus <null>
2021-08-19 09:51:22 -0400

    removed reused code

--- c2/m_c2.v
+++ c2/m_c2.v
@@ -9,17 +9,6 @@ import rand
 import net
 // detect os
 
-struct Windows_C2_Config {
-
-}
-
-struct Linux_C2_Config {
-
-}
-
-pub fn get_os() string {
-	return os.user_os()
-}
 fn handle_linux_c2() bool {
 	term.clear()
 	println("running on linux...")
@@ -27,6 +16,20 @@ fn handle_linux_c2() bool {
 	results := linux.get_ip_addresses() or {
 		panic(err)
 	}
+	return handle_c2(results)
+	
+}
+fn handle_windows_c2() bool {
+	term.clear()
+	println("running on windows...")
+	println("=====================")
+	results := windows.get_ip_addresses() or {
+		panic(err)
+	}
+	return handle_c2(results)
+}
+
+fn handle_c2(results []string) bool {
 	println("Which IP would you like to listen on?")
 	println("0). Back")
 	for idx, ip in results {
@@ -41,36 +44,6 @@ fn handle_linux_c2() bool {
 		return false
 	}
 	choice = choice - 1
-	term.clear()
-	println("Selected Interface with IP: ${results[choice]}")
-	port_choice := strconv.atoi(os.input("What port would you like to listen on? [Press enter for random high]")) or {
-		rand.int_in_range(50000,65535)
-	}
-	term.clear()
-	println("Starting listener on ${results[choice]}:$port_choice")
-	create_listener(results[choice], port_choice)
-	return true
-}
-fn handle_windows_c2() bool {
-	term.clear()
-	println("running on windows...")
-	println("=====================")
-	results := windows.get_ip_addresses() or {
-		panic(err)
-	}
-	println("Which IP would you like to listen on?")
-	println("0). Back")
-	for idx, ip in results {
-		println("${idx+1}). $ip")
-	}
-	choice := strconv.atoi(os.input("Input interface number: ")) or { 777 }
-	if choice == 0 {
-		term.clear()
-		return true
-	}
-	if choice == 777 {
-		return false
-	}
 	term.clear()
 	println("Selected Interface with IP: ${results[choice]}")
 	port_choice := strconv.atoi(os.input("What port would you like to listen on? [Press enter for random high]")) or {
@@ -108,7 +81,6 @@ pub fn create_listener(ip string, port int) {
 }
 
 fn handle_listener(mut c net.UdpConn, conn_data chan []byte) {
-
 	for {
 		mut buf := []byte{len: 100, init: 0}
 		read, addr := c.read(mut buf) or { continue }
@@ -128,7 +100,7 @@ fn handle_listener(mut c net.UdpConn, conn_data chan []byte) {
 }
 
 pub fn start_c2() bool {
-	match get_os() {
+	match os.user_os() {
 		"windows" {
 			return handle_windows_c2()
 		}
@@ -136,7 +108,7 @@ pub fn start_c2() bool {
 			return handle_linux_c2()
 		}
 		else {
-			println("unsupported os")
+			println("unsupported os, will try to add MacOS in the future")
 			return false
 		}
 	}
